cf7dd663156648b6890297b8d7071ec3625c94bd
ibeizhu <ibeizhu@gmail.com>
2025-07-18 16:43:55 +0800

    Updates

---
+++ docs__api.en-US.md.3a0980d8.async.js
@@ -0,0 +1,66 @@
+"use strict";(self.webpackChunkreact_secure_state=self.webpackChunkreact_secure_state||[]).push([[647],{45105:function(l,t,s){s.r(t);var d=s(72269),u=s(93359),_=s(61788),x=s(19977),h=s(25809),n=s(90978),v=s(96057),p=s(83213),i=s(53683),r=s(80936),o=s(67294),a=s(51205),e=s(85893);function c(){return(0,e.jsx)(i.dY,{children:(0,e.jsx)(o.Suspense,{fallback:(0,e.jsx)(r.Z,{}),children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h1",{id:"api",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#api",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"API"]}),(0,e.jsxs)("h2",{id:"createstore",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#createstore",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"createStore"]}),(0,e.jsxs)("p",{children:[(0,e.jsx)("code",{children:a.texts[0].value}),a.texts[1].value]}),(0,e.jsxs)("p",{children:[a.texts[2].value,(0,e.jsx)("code",{children:a.texts[3].value}),a.texts[4].value,(0,e.jsx)("code",{children:a.texts[5].value}),a.texts[6].value]}),(0,e.jsxs)("h3",{id:"type-definition",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#type-definition",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Type Definition"]}),(0,e.jsx)(n.Z,{lang:"tsx",children:a.texts[7].value}),(0,e.jsxs)("h2",{id:"storeprovider",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#storeprovider",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"StoreProvider"]}),(0,e.jsxs)("p",{children:[(0,e.jsx)("code",{children:a.texts[8].value}),a.texts[9].value]}),(0,e.jsxs)("h3",{id:"type-definition-1",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#type-definition-1",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Type Definition"]}),(0,e.jsx)(n.Z,{lang:"tsx",children:a.texts[10].value}),(0,e.jsxs)("ul",{children:[(0,e.jsxs)("li",{children:[(0,e.jsx)("code",{children:a.texts[11].value}),a.texts[12].value]}),(0,e.jsxs)("li",{children:[(0,e.jsx)("code",{children:a.texts[13].value}),a.texts[14].value]})]}),(0,e.jsxs)("h3",{id:"usage",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#usage",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Usage"]}),(0,e.jsx)(n.Z,{lang:"tsx",children:a.texts[15].value}),(0,e.jsxs)("h2",{id:"usestorevalues",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#usestorevalues",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"useStoreValues"]}),(0,e.jsxs)("p",{children:[a.texts[16].value,(0,e.jsx)("code",{children:a.texts[17].value}),a.texts[18].value]}),(0,e.jsxs)("h3",{id:"type-definition-2",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#type-definition-2",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Type Definition"]}),(0,e.jsx)(n.Z,{lang:"tsx",children:a.texts[19].value}),(0,e.jsx)("p",{children:a.texts[20].value}),(0,e.jsx)("ul",{children:(0,e.jsxs)("li",{children:[(0,e.jsx)("code",{children:a.texts[21].value}),(0,e.jsx)("ul",{children:(0,e.jsx)("li",{children:a.texts[22].value})})]})}),(0,e.jsx)("p",{children:a.texts[23].value}),(0,e.jsxs)("ul",{children:[(0,e.jsxs)("li",{children:[(0,e.jsx)("code",{children:a.texts[24].value}),(0,e.jsx)("ul",{children:(0,e.jsx)("li",{children:a.texts[25].value})})]}),(0,e.jsxs)("li",{children:[(0,e.jsx)("code",{children:a.texts[26].value}),(0,e.jsxs)("ul",{children:[(0,e.jsx)("li",{children:a.texts[27].value}),(0,e.jsxs)("li",{children:[(0,e.jsx)("code",{children:a.texts[28].value}),a.texts[29].value]})]})]}),(0,e.jsxs)("li",{children:[(0,e.jsx)("code",{children:a.texts[30].value}),(0,e.jsxs)("ul",{children:[(0,e.jsx)("li",{children:a.texts[31].value}),(0,e.jsxs)("li",{children:[(0,e.jsx)("code",{children:a.texts[32].value}),a.texts[33].value]})]})]})]}),(0,e.jsxs)("h3",{id:"usage-1",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#usage-1",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Usage"]}),(0,e.jsx)(n.Z,{lang:"tsx",children:a.texts[34].value}),(0,e.jsxs)("h2",{id:"typical-usage",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#typical-usage",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Typical Usage"]}),(0,e.jsx)(n.Z,{lang:"tsx",children:a.texts[35].value})]})})})})}t.default=c},51205:function(l,t,s){s.r(t),s.d(t,{texts:function(){return d}});const d=[{value:"createStore",paraId:0,tocIndex:1},{value:" is a factory function for generating a React context for managing data of a specified object type, used for global state management within an application.",paraId:0,tocIndex:1},{value:"It provides a ",paraId:1,tocIndex:1},{value:"StoreProvider",paraId:1,tocIndex:1},{value:" component and a ",paraId:1,tocIndex:1},{value:"useStoreValues",paraId:1,tocIndex:1},{value:" Hook.",paraId:1,tocIndex:1},{value:`function createStore<T extends Record<string, any>>(): {
+  StoreProvider: React.FC<StoreProviderProps<T>>;
+  useStoreValues: <K extends FieldPath<T>>(
+    fields: K[],
+  ) => UseStoreValuesReturn<T, K>;
+};
+`,paraId:2,tocIndex:2},{value:"StoreProvider",paraId:3,tocIndex:3},{value:" provides a context layer, where data is stored in memory and can be accessed and updated later via useStoreValues.",paraId:3,tocIndex:3},{value:`interface StoreProviderProps<T> {
+  initialValue: T;
+  children: React.ReactNode;
+}
+`,paraId:4,tocIndex:4},{value:"initialValue: T",paraId:5,tocIndex:4},{value:": The initial value of the Store, must be an object.",paraId:5,tocIndex:4},{value:"children: React.ReactNode",paraId:5,tocIndex:4},{value:": React.ReactNode: The child components to be wrapped.",paraId:5,tocIndex:4},{value:`<StoreProvider initialValue={yourInitialData}>
+  {/* your components */}
+</StoreProvider>
+`,paraId:6,tocIndex:5},{value:"Used to access data from Store. ",paraId:7,tocIndex:6},{value:"useStoreValues",paraId:7,tocIndex:6},{value:" strictly controls permission at the field level.",paraId:7,tocIndex:6},{value:`function useStoreValues<K extends FieldPath<T>>(
+  fields: K[],
+): {
+  values: Pick<T, K>;
+  setFieldValue: (path: K, value: any) => void;
+  setFieldsValue: (fieldsValue: Partial<Record<FieldPath<T>, any>>) => void;
+};
+`,paraId:8,tocIndex:7},{value:"Parameters",paraId:9,tocIndex:7},{value:"fields: K[]",paraId:10,tocIndex:7},{value:"An array of the field names to subscribe and operate. Only these fields will be monitored and exposed.",paraId:11,tocIndex:7},{value:"Return object",paraId:12,tocIndex:7},{value:"values: Pick<T, K>",paraId:13,tocIndex:7},{value:"The object with the values of the currently subscribed fields. For example: { fieldA: valueA, fieldB: valueB }.",paraId:14,tocIndex:7},{value:"setFieldValue: (path: K, value: any) => void",paraId:13,tocIndex:7},{value:"Update the value of a specific field. Only fields registered in fields can be updated, otherwise an error will be thrown.",paraId:15,tocIndex:7},{value:"useStoreValues",paraId:15,tocIndex:7},{value:" strictly controls data read/write permissions; unrequested fields cannot be modified.",paraId:15,tocIndex:7},{value:"setFieldsValue: (fieldsValue: Partial<Record<FieldPath<T>, any>>) => void",paraId:13,tocIndex:7},{value:"Batch update values of multiple fields. Only fields registered in fields can be updated, otherwise an error will be thrown.",paraId:16,tocIndex:7},{value:"useStoreValues",paraId:16,tocIndex:7},{value:" strictly controls data read/write permissions; unrequested fields cannot be modified.",paraId:16,tocIndex:7},{value:`const { values, setFieldValue, setFieldsValue } = useStoreValues([
+  'fieldA',
+  'fieldB',
+]);
+
+useEffect(() => {
+  // Access value
+  console.log(values.fieldA);
+}, [values]);
+
+// Set a single field
+setFieldValue('fieldA', 'newValueA');
+
+// Batch set
+setFieldsValue({ fieldA: 123, fieldB: 'abc' });
+`,paraId:17,tocIndex:8},{value:`// Define type
+type MyStore = {
+  count: number;
+  user: string;
+};
+
+// Create store
+const { StoreProvider, useStoreValues } = createStore<MyStore>();
+
+// At app root
+<StoreProvider initialValue={{ count: 0, user: '' }}>
+  <MyComponent />
+</StoreProvider>;
+
+// In a component
+function MyComponent() {
+  const { values, setFieldValue, setFieldsValue } = useStoreValues([
+    'count',
+    'user',
+  ]);
+
+  return (
+    <div>
+      <span>{values.count}</span>
+      <button onClick={() => setFieldValue('count', values.count + 1)}>
+        +
+      </button>
+    </div>
+  );
+}
+`,paraId:18,tocIndex:9}]}}]);
--- meta__docs__en-US.42bbd150.async.js
+++ meta__docs__en-US.42bbd150.async.js
@@ -1,7 +1,9 @@
 "use strict";(self.webpackChunkreact_secure_state=self.webpackChunkreact_secure_state||[]).push([[430],{96399:function(t,n,e){e.r(n),e.d(n,{demos:function(){return o}});var a=e(67294),o={}},23129:function(t,n,e){e.r(n),e.d(n,{demos:function(){return o}});var a=e(67294),o={}},82908:function(t,n,e){e.r(n),e.d(n,{demos:function(){return o}});var a=e(67294),o={}},51205:function(t,n,e){e.r(n),e.d(n,{texts:function(){return a}});const a=[{value:"createStore",paraId:0,tocIndex:1},{value:" is a factory function for generating a React context for managing data of a specified object type, used for global state management within an application.",paraId:0,tocIndex:1},{value:"It provides a ",paraId:1,tocIndex:1},{value:"StoreProvider",paraId:1,tocIndex:1},{value:" component and a ",paraId:1,tocIndex:1},{value:"useStoreValues",paraId:1,tocIndex:1},{value:" Hook.",paraId:1,tocIndex:1},{value:`function createStore<T extends Record<string, any>>(): {
   StoreProvider: React.FC<StoreProviderProps<T>>;
-  useStoreValues: <K extends FieldPath<T>>(fields: K[]) => UseStoreValuesReturn<T, K>;
-}
+  useStoreValues: <K extends FieldPath<T>>(
+    fields: K[],
+  ) => UseStoreValuesReturn<T, K>;
+};
 `,paraId:2,tocIndex:2},{value:"StoreProvider",paraId:3,tocIndex:3},{value:" provides a context layer, where data is stored in memory and can be accessed and updated later via useStoreValues.",paraId:3,tocIndex:3},{value:`interface StoreProviderProps<T> {
   initialValue: T;
   children: React.ReactNode;
@@ -9,12 +11,17 @@
 `,paraId:4,tocIndex:4},{value:"initialValue: T",paraId:5,tocIndex:4},{value:": The initial value of the Store, must be an object.",paraId:5,tocIndex:4},{value:"children: React.ReactNode",paraId:5,tocIndex:4},{value:": React.ReactNode: The child components to be wrapped.",paraId:5,tocIndex:4},{value:`<StoreProvider initialValue={yourInitialData}>
   {/* your components */}
 </StoreProvider>
-`,paraId:6,tocIndex:5},{value:"Used to access data from Store. ",paraId:7,tocIndex:6},{value:"useStoreValues",paraId:7,tocIndex:6},{value:" strictly controls permission at the field level.",paraId:7,tocIndex:6},{value:`function useStoreValues<K extends FieldPath<T>>(fields: K[]): {
+`,paraId:6,tocIndex:5},{value:"Used to access data from Store. ",paraId:7,tocIndex:6},{value:"useStoreValues",paraId:7,tocIndex:6},{value:" strictly controls permission at the field level.",paraId:7,tocIndex:6},{value:`function useStoreValues<K extends FieldPath<T>>(
+  fields: K[],
+): {
   values: Pick<T, K>;
   setFieldValue: (path: K, value: any) => void;
   setFieldsValue: (fieldsValue: Partial<Record<FieldPath<T>, any>>) => void;
-}
-`,paraId:8,tocIndex:7},{value:"Parameters",paraId:9,tocIndex:7},{value:"fields: K[]",paraId:10,tocIndex:7},{value:"An array of the field names to subscribe and operate. Only these fields will be monitored and exposed.",paraId:11,tocIndex:7},{value:"Return object",paraId:12,tocIndex:7},{value:"values: Pick<T, K>",paraId:13,tocIndex:7},{value:"The object with the values of the currently subscribed fields. For example: { fieldA: valueA, fieldB: valueB }.",paraId:14,tocIndex:7},{value:"setFieldValue: (path: K, value: any) => void",paraId:13,tocIndex:7},{value:"Update the value of a specific field. Only fields registered in fields can be updated, otherwise an error will be thrown.",paraId:15,tocIndex:7},{value:"useStoreValues",paraId:15,tocIndex:7},{value:" strictly controls data read/write permissions; unrequested fields cannot be modified.",paraId:15,tocIndex:7},{value:"setFieldsValue: (fieldsValue: Partial<Record<FieldPath<T>, any>>) => void",paraId:13,tocIndex:7},{value:"Batch update values of multiple fields. Only fields registered in fields can be updated, otherwise an error will be thrown.",paraId:16,tocIndex:7},{value:"useStoreValues",paraId:16,tocIndex:7},{value:" strictly controls data read/write permissions; unrequested fields cannot be modified.",paraId:16,tocIndex:7},{value:`const { values, setFieldValue, setFieldsValue } = useStoreValues(['fieldA', 'fieldB']);
+};
+`,paraId:8,tocIndex:7},{value:"Parameters",paraId:9,tocIndex:7},{value:"fields: K[]",paraId:10,tocIndex:7},{value:"An array of the field names to subscribe and operate. Only these fields will be monitored and exposed.",paraId:11,tocIndex:7},{value:"Return object",paraId:12,tocIndex:7},{value:"values: Pick<T, K>",paraId:13,tocIndex:7},{value:"The object with the values of the currently subscribed fields. For example: { fieldA: valueA, fieldB: valueB }.",paraId:14,tocIndex:7},{value:"setFieldValue: (path: K, value: any) => void",paraId:13,tocIndex:7},{value:"Update the value of a specific field. Only fields registered in fields can be updated, otherwise an error will be thrown.",paraId:15,tocIndex:7},{value:"useStoreValues",paraId:15,tocIndex:7},{value:" strictly controls data read/write permissions; unrequested fields cannot be modified.",paraId:15,tocIndex:7},{value:"setFieldsValue: (fieldsValue: Partial<Record<FieldPath<T>, any>>) => void",paraId:13,tocIndex:7},{value:"Batch update values of multiple fields. Only fields registered in fields can be updated, otherwise an error will be thrown.",paraId:16,tocIndex:7},{value:"useStoreValues",paraId:16,tocIndex:7},{value:" strictly controls data read/write permissions; unrequested fields cannot be modified.",paraId:16,tocIndex:7},{value:`const { values, setFieldValue, setFieldsValue } = useStoreValues([
+  'fieldA',
+  'fieldB',
+]);
 
 useEffect(() => {
   // Access value
@@ -38,16 +45,21 @@ const { StoreProvider, useStoreValues } = createStore<MyStore>();
 // At app root
 <StoreProvider initialValue={{ count: 0, user: '' }}>
   <MyComponent />
-</StoreProvider>
+</StoreProvider>;
 
 // In a component
 function MyComponent() {
-  const { values, setFieldValue, setFieldsValue } = useStoreValues(['count', 'user']);
+  const { values, setFieldValue, setFieldsValue } = useStoreValues([
+    'count',
+    'user',
+  ]);
 
   return (
     <div>
       <span>{values.count}</span>
-      <button onClick={() => setFieldValue('count', values.count + 1)}>+</button>
+      <button onClick={() => setFieldValue('count', values.count + 1)}>
+        +
+      </button>
     </div>
   );
 }
